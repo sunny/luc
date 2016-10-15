e85ad609a8990d6c9d872559fffaefd65ac7d9f5
Sunny Ripert <sunny@sunfox.org>
2016-10-15 22:31:06 +0200

    Add more commands and giphy extras

--- cults_api.rb
+++ cults_api.rb
@@ -1,22 +1,32 @@
 module Luc
   class CultsAPI
     def latest_creation
-      http.get("http://cults3d.com/api/v1/creations.json?limit=1")
-          .parse
-          .first
+      latest_creations(limit: 1).first
     end
 
     def most_downloaded_creation
-      http.get("http://cults3d.com/api/v1/creations.json?limit=1&sort=sales_counter")
-          .parse
-          .first
+      most_downloaded_creations(limit: 1).first
     end
 
     def most_liked_creation
-      http.get("http://cults3d.com/api/v1/creations.json?limit=1&sort=likes_counter")
-          .parse
-          .first
+      most_liked_creations(limit: 1).first
     end
+
+    def latest_creations(limit:)
+      http.get("http://cults3d.com/api/v1/creations.json?limit=#{limit}")
+          .parse
+    end
+
+    def most_downloaded_creations(limit:)
+      http.get("http://cults3d.com/api/v1/creations.json?limit=#{limit}&sort=sales_counter")
+          .parse
+    end
+
+    def most_liked_creations(limit:)
+      http.get("http://cults3d.com/api/v1/creations.json?limit=#{limit}&sort=likes_counter")
+          .parse
+    end
+
 
     private
 
--- luc_bot.rb
+++ luc_bot.rb
@@ -6,35 +6,88 @@ require "meuh/slack_plugin"
 
 module Luc
   class Bot < SlackRubyBot::Bot
+    command "help" do |client, data, match|
+      client.say(
+        channel: data.channel,
+        text: [
+          "Commands:",
+          "*@luc <num> latest creations* - Latest creations on Cults",
+          "*@luc <num> most liked creations* - Most liked creations on Cults",
+          "*@luc <num> most downloaded creations* - Most downloaded creations on Cults",
+        ]
+      )
+    end
+
     command "latest creation" do |client, data, match|
+      client.typing(channel: data.channel)
       creation = CultsAPI.new.latest_creation
       creator = creation["creator"]
       client.say(
         channel: data.channel,
-        text: "The latest creation is *#{creation["name"]}* " \
-              "by *#{creator["nick"]}*: #{creation["url"]} :new:"
+        text: ":new: The latest creation on :cults: is *#{creation["name"]}* " \
+              "by *#{creator["nick"]}*: #{creation["url"]}"
+      )
+    end
+
+    match(/(?<bot>[[:alnum:][:punct:]@<>]*)\s(?<count>\d+) latest creations$/) do |client, data, match|
+      client.typing(channel: data.channel)
+      count = match["count"].to_i
+      creations = CultsAPI.new.latest_creations(limit: count)
+      client.say(
+        channel: data.channel,
+        text: [":new: The #{count} latest creations on :cults: are:",
+               creations.map { |creation| "- #{creation["url"]}" }].compact,
+        giphy: "yay"
       )
     end
 
     command "most downloaded creation" do |client, data, match|
+      client.typing(channel: data.channel)
       creation = CultsAPI.new.most_downloaded_creation
       creator = creation["creator"]
       client.say(
         channel: data.channel,
-        text: "The most downloaded creation is *#{creation["name"]}* " \
-              "by *#{creator["nick"]}*: #{creation["url"]} :champagne:"
+        text: ":champagne: The most downloaded creation on :cults: is " \
+              "*#{creation["name"]}* by *#{creator["nick"]}*: " \
+              "#{creation["url"]}"
+      )
+    end
+
+    match(/^(?<bot>[[:alnum:][:punct:]@<>]*)\s(?<count>\d+) most downloaded creations$/) do |client, data, match|
+      client.typing(channel: data.channel)
+      count = match["count"].to_i
+      creations = CultsAPI.new.most_downloaded_creations(limit: count)
+      client.say(
+        channel: data.channel,
+        text: [":champagne: The #{count} most downloaded creations on :cults: are:",
+               creations.map { |creation| "- #{creation["url"]}" }].compact,
+        gif: "download"
       )
     end
 
     command "most liked creation" do |client, data, match|
+      client.typing(channel: data.channel)
       creation = CultsAPI.new.most_liked_creation
       creator = creation["creator"]
       client.say(
         channel: data.channel,
-        text: "The most liked creation is *#{creation["name"]}* " \
-              "by *#{creator["nick"]}*: #{creation["url"]} :heart:"
+        text: ":heart: The most liked creation on :cults: is *#{creation["name"]}* " \
+              "by *#{creator["nick"]}*: #{creation["url"]}"
       )
     end
+
+    match(/^(?<bot>[[:alnum:][:punct:]@<>]*)\s(?<count>\d+) most liked creations$/) do |client, data, match|
+      client.typing(channel: data.channel)
+      count = match["count"].to_i
+      creations = CultsAPI.new.most_liked_creations(limit: count)
+      client.say(
+        channel: data.channel,
+        text: [":heart: The #{count} most liked creations on :cults: are:",
+               creations.map { |creation| "- #{creation["url"]}" }].compact,
+        giphy: "like"
+      )
+    end
+
 
     extend Meuh::SlackPlugin
   end
